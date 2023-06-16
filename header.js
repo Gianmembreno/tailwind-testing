353cc7d8be722e243d1253ff0f0e8132eed908cf
Gianmembreno <66712565+Gianmembreno@users.noreply.github.com>
2023-06-15 21:45:03 -0400

    deleting

--- header.js
+++ header.js
@@ -1,14 +1,3 @@
 const header = () => {};
 
 export default header;
-
-// Path: footer.js
-// const footer = () => {};
-// export default footer;
-
-// // Path: index.js
-// import header from './header';
-// import footer from './footer';
-
-// header();
-// footer();
