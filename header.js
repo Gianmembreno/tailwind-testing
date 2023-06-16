7c83fe755e91f0f208cc5661cc3779fd3517eda4
Gianmembreno <66712565+Gianmembreno@users.noreply.github.com>
2023-06-15 21:44:53 -0400

    testing

--- header.js
+++ header.js
@@ -1,3 +1,14 @@
 const header = () => {};
 
 export default header;
+
+// Path: footer.js
+// const footer = () => {};
+// export default footer;
+
+// // Path: index.js
+// import header from './header';
+// import footer from './footer';
+
+// header();
+// footer();
